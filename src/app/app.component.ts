58e019e1a7bc2063f1a1db10045490fcf96fce45
elilabourne <elilabourne.detorres@gmail.com>
2022-03-22 10:31:39 +0800

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,44 @@
 import { Component } from '@angular/core';
+import * as UaParser from 'ua-parser-js';
 
+interface Device { 
+  model: String,
+  vendor: String, 
+  type: String
+}
+
+interface OS {
+  name: String,
+  version: String
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'ua-parser';
+
+  device: Device;
+  os: OS;
+  ngOnInit(){
+    const parser = new UaParser();
+    const parsed = parser.getResult();
+    console.log(parsed);
+    
+    this.device = {
+      model: parsed.device.model === undefined ? (parsed.browser.name === 'Chrome') ? 'Desktop' : '' : parsed.device.model,
+      vendor: parsed.device.vendor === undefined ? (parsed.browser.name === 'Chrome') ? 'Desktop': '' : parsed.device.vendor,
+      type: parsed.device.type === undefined ? (parsed.browser.name === 'Chrome') ? 'Desktop': '' : parsed.device.type
+    }
+
+    this.os = {
+      name: parsed.os.name,
+      version: parsed.os.version
+    }
+  }
+
+  reload(){
+    location.reload();
+  }
 }
